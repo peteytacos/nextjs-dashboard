889ada5d2d6c0c1545521f5274f5e7b3935ff0c2
Pete Wallroth <p.wallroth@yahoo.com>
2024-03-31 11:59:44 +1100

    Add customers table to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from 'next';
+import { nunito } from '@/app/ui/fonts';
+import CustomersTable from '@/app/ui/customers/table'
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
 export default function Page() {
-    return <p>Customers Page</p>;
+    return (
+        <main>
+            {/* <h1 className={`${nunito.className} mb-4 text-xl md:text-2xl`}>
+                Customers
+            </h1> */}
+            <div className="flex w-full items-center justify-between">
+                <CustomersTable customers={[]} />
+            </div>
+        </main >
+    );
 }
