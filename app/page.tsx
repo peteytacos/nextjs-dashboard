2fa3fd5d1db2dba46ca767e64983f0beffc110bd
Pete Wallroth <p.wallroth@yahoo.com>
2024-03-31 11:21:13 +1100

    Update copy on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel and Pete.˝
+            , brought to you by Vercel and Pete.
           </p>
           <Link
             href="/login"
